5a95cd7a46317dfb768e693bf1621d159aaf84ea
Neil Manvar <ndmanvar@yahoo.com>
2017-10-23 22:48:40 -0700

    add features column and move errors to left
    Also contains other minor formatting / layout improvements such as color change on click.

--- src/components/Errors.js
+++ src/components/Errors.js
@@ -7,8 +7,11 @@ import logo from '../assets/sentry-glyph-black.png';
 class Errors extends Component {
     constructor(props) {
         super(props);
+        this.state = {color: 'black'};
+        Raven.setTagsContext({page: "ErrorsPage"});
     }
 
+    // ERRORS
     notAFunctionError() {
         var obj = {};
         obj.attributeInvalid();
@@ -37,24 +40,52 @@ class Errors extends Component {
     evalError() {
         throw new EvalError('Hello', 'someFile.js', 10);
     }
+    // ERRORS (end)
+
+    showError(color) {
+        var that = this;
+        that.setState({color: color});
+        setTimeout(function () {
+            that.setState({color: 'black'});
+        }, 1500);
+    }
+
+    // RAVEN CONFIGURATIONS:
+    setSampleTag() {
+        Raven.setTagsContext({sampleTag: "sampleValue"});
+    }
+
+    setExtraContext() {
+        Raven.setExtraContext({ foo: "bar" });
+    }
+    // RAVEN CONFIGURATIONS (end)
 
     render() {
         const user = this.props.location.state;
 
         return (
-            <div>
-                <h2>Hi, {user ? user.email : "Guest"}.</h2>
-                <p>Welcome the list of sample errors. <br/>
-                This is where you can specify your DSN and send errors/exceptions to Sentry.</p>
+            <div style={{
+                color: this.state.color
+            }}>
+                <h2>Hi, {user
+                        ? user.email
+                        : "Guest"}.</h2>
+                <p>Welcome the list of sample errors.
+                    <br/>
+                    This is where you can specify your DSN and send errors/exceptions to Sentry.</p>
                 <div>
-                    <ul className="center list-group">
+                    <ul className="left list-group " onClick={this.showError.bind(this, 'red')}>
+                        <li className="list-group-item list-group-item-danger">
+                            <h3>ERRORS</h3>
+                        </li>
                         <li className="list-group-item" onClick={this.notAFunctionError}>
                             <h3>TypeError</h3>
                             <p>obj.attributeInvalid is not a function</p>
                         </li>
                         <li className="list-group-item" onClick={this.uriError}>
                             <h3>URIError</h3>
-                            <p>URI malformed </p>
+                            <p>URI malformed
+                            </p>
                         </li>
                         <li className="list-group-item" onClick={this.typeError}>
                             <h3>Uncaught TypeError</h3>
@@ -75,6 +106,20 @@ class Errors extends Component {
                         <li className="list-group-item" onClick={this.evalError}>
                             <h3>EvalError</h3>
                             <p>Hello</p>
+                        </li>
+                    </ul>
+
+                    <ul className="left list-group" onClick={this.showError.bind(this, 'green')}>
+                        <li className="list-group-item list-group-item-success">
+                            <h3>FEATURES</h3>
+                        </li>
+                        <li className="list-group-item" onClick={this.setSampleTag}>
+                            <h3>Set Sample Tag</h3>
+                            <p>{'Raven.setTagsContext({sampleTag: "sampleValue"});'}</p>
+                        </li>
+                        <li className="list-group-item" onClick={this.setExta}>
+                            <h3>Set Sample Tag</h3>
+                            <p>{'Raven.setExtraContext({foo: "bar"});'}</p>
                         </li>
                     </ul>
                 </div>
