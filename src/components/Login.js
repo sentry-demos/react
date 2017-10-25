c0c11968e4a20e607efd5fba1b3b24a1f89cb1da
Neil Manvar <ndmanvar@yahoo.com>
2017-10-25 15:46:04 -0700

    introduce sentry

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,6 +17,7 @@ class Login extends Component {
 
     handleSubmit(event) {
         var email = this.state.email;
+        Raven.setUserContext({email});
         this.props.history.pushState({email}, "/errors");
     }
 
