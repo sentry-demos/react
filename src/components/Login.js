01c16cc1d96463e20d9c601942e3757535bd007e
Neil Manvar <ndmanvar@yahoo.com>
2017-11-21 09:49:48 -0800

    Revert "remove raven from login"
    This reverts commit 5b909c2359fd42e43a2d7181764cdc2596966d1b.

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ class Login extends Component {
 
     handleSubmit(event) {
         var email = this.state.email;
-        // Raven.setUserContext({email});
+        Raven.setUserContext({email});
         this.props.history.pushState({email}, "/errors");
     }
 
