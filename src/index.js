c3ab2fe570cbfd41eb7331cba89333219a25c6ea
Neil Manvar <neil@sentry.io>
2018-04-23 17:19:27 -0700

    got settings router working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 
 import './index.css';
 import App from './components/App';
+import Errors from './components/Errors';
+import Settings from './components/Settings';
 import registerServiceWorker from './registerServiceWorker';
 
 
 const app = document.getElementById('root')
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+<Router history={hashHistory}>
+    <Route path="/" component={App}>
+        <IndexRoute component={Errors}></IndexRoute>
+        <Route path="settings" name="settings" component={Settings}></Route>
+    </Route>
+</Router>,
+app);
+
 registerServiceWorker();
