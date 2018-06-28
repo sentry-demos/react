356b5b567ac0ea95457063c4b8ac1861678b74eb
Neil Manvar <neil@sentry.io>
2018-06-28 09:28:15 -0700

    Add validFunction
    Fixes REACT-DEMO-D3-P

--- src/components/Errors.js
+++ src/components/Errors.js
@@ -31,8 +31,10 @@ class Errors extends Component {
 
     // ERRORS
     notAFunctionError() {
-        var obj = {};
-        obj.invalidFunction();
+        var obj = {
+            validFunction: function () {}
+        };
+        obj.validFunction();
     }
 
     uriError() {
