768d9854c778f4084a4cb66ec5cd6f8b2ad6f697
Neil Manvar <neil@sentry.io>
2018-11-12 15:57:22 -0800

    Add hardware store demo

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
-
-
 import './index.css';
 import App from './components/App';
-import Errors from './components/Errors';
-import Checkout from './components/Checkout';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const app = document.getElementById('root')
-ReactDOM.render(
-<Router history={hashHistory}>
-    <Route path="/" component={App}>
-        <IndexRoute component={Errors}></IndexRoute>
-        <Route path="checkout" name="checkout" component={Checkout}></Route>
-    </Route>
-</Router>,
-app);
+ReactDOM.render(<App /> , document.getElementById('root'));
 
 registerServiceWorker();
