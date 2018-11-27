6dc8e3b00fbab25bc3a59071a1a20f4af69a6c4d
Neil Manvar <neil@sentry.io>
2018-11-26 16:23:46 -0800

    Revert "Add hammer to inventory"
    This reverts commit e41d20898e3845c261b62298ee059fbd8015a0f6.

--- src/components/App.js
+++ src/components/App.js
@@ -19,9 +19,6 @@ class App extends Component {
       },
       screwdriver: {
         price: 5
-      },
-      hammer: {
-        price: 10
       }
     };
     this.buyItem = this.buyItem.bind(this);
@@ -33,8 +30,6 @@ class App extends Component {
         scope.setUser({ email: this.email}); // attach user/email context
         scope.setTag("customerType", "medium-plan"); // custom-tag
     });
-
-    Sentry.captureMessage('abczzz');
   }
 
   buyItem(item) {
