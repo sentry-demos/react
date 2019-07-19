901764f8fe35c35a3e6d394b711b462008910cb7
Ido Shemesh <ido.shemesh001@gmail.com>
2019-07-19 13:12:17 -0700

    add random txt file

--- src/components/App.js
+++ src/components/App.js
@@ -103,7 +103,7 @@ class App extends Component {
   performXHRRequest(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
            console.log( xhttp.responseText);
         }
     };
