10457a212765501abe4e9e1447d2d82f1cf50778
William Capozzoli <thinkocapo@gmail.com>
2019-08-04 19:03:46 -0700

    Boolean evaluation of arg

--- demo.sh
+++ demo.sh
@@ -12,9 +12,14 @@ NET=1
 
 # TODO
 # if $1 = 'flask' then PORT=5001 npm run deploy
+if [[ $1 == 'flask' ]]
+then echo 'ITS FLASK';
+fi
 # if $1 = 'rails' then PORT=8000 npm run deploy
 # if $1 = '.net' then PORT=8080
 # if $1 = 'laravel' then PORT=
 # if $1 = 'spring' then PORT=
 
 # first echo that it recognizes/reads the if-then statement correctly.
+
+echo 'done';
