98c4e739f24c9e643892bddd263398b0f09c047f
William Capozzoli <thinkocapo@gmail.com>
2019-08-12 13:28:34 -0700

    back-end commands

--- react.sh
+++ react.sh
@@ -2,26 +2,26 @@
 #  Example usage:
 # ./react.sh express
 
-if [[ $1 == 'aspnet' ]]
+if [[ $1 == 'aspnet' ]] # SENTRY_RELEASE=`sentry-cli releases propose-version` pwsh deploy.ps1
     then REACT_APP_PORT=5001 npm run deploy;
 fi
 
-if [[ $1 == 'express' ]]
+if [[ $1 == 'express' ]] # npm run deploy
     then REACT_APP_PORT=3001 npm run deploy;
 fi
 
-if [[ $1 == 'flask' ]]
+if [[ $1 == 'flask' ]] # make deploy
     then REACT_APP_PORT=5001 npm run deploy;
 fi
 
-if [[ $1 == 'laravel' ]]
+if [[ $1 == 'laravel' ]] # make
     then REACT_APP_PORT=8000 npm run deploy;
 fi
 
-if [[ $1 == 'rails' ]]
+if [[ $1 == 'rails' ]] # make deploy
     then REACT_APP_PORT=3001 npm run deploy;
 fi
 
-if [[ $1 == 'spring' ]]
+if [[ $1 == 'spring' ]] # make
     then REACT_APP_PORT=8080 npm run deploy;
 fi
