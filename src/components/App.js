851582cf2fc53ae7b10246d260e60d393826462a
William Capozzoli <thinkocapo@gmail.com>
2019-08-12 17:02:52 -0700

    WithScope for Transaction Id (#71)
    * first update
    
    * testing
    
    * first commit on dev branch
    
    * log
    
    * stable
    
    * update
    
    * return null to test it doesnt send
    
    * event, hint
    
    * update
    
    * IP address
    
    * test scrubbing password from Tags
    
    * test extra
    
    * test Subject Template email setting
    
    * removed cart tag scrub test, was causing problem
    
    * Sentry test send a Message captureMessage
    
    * associate commits again in Makefile now that Integration was fixed
    
    * test a commit
    
    * comment out echo VERSION
    
    * iWantString gets a string, start of Suspect Commit Test
    
    * no more broken code (no Sentry errors). finish feature/PR
    
    * reset demo
    
    * sentry init
    
    * checkout cleaned. renames
    
    * update
    
    * update
    
    * update
    
    * little updates
    
    * set user
    
    * update
    
    * update
    
    * update
    
    * sentry cdn Tracing bundle. updates
    
    * Sentry Integrations Tracing constructor
    
    * tracingOrigins param
    
    * attempts
    
    * tracing updates
    
    * cleanup
    
    * showReportDialog
    
    * deleted token and comment
    
    * test
    
    * untest
    
    * revert X-Transaction-ID
    
    * updates for Tracing testing of Headers with java back-end
    
    * update port for Tracing headers test with java spring, log4j2
    
    * fixes
    
    * Fixes WILL-FRONTEND-REACT-Y
    
    * Fixes WILL-FRONTEND-REACT-4
    
    * commit1
    
    * commit2
    
    * restore demo
    
    * sdk 5.4.1 fixes headers, test it
    
    * reinstated old tracing_id technique
    
    * native tracting tested, removed sessionId and unecessary headers
    
    * readme note on master vs tracing-integration
    
    * restored lost comments
    
    * restored and tested X-transaction-ID old tracing functionality
    
    * PORT set with npm command
    
    * README instructions for demo.sh
    
    * Boolean evaluation of arg
    
    * renamed PORT due to react using PORT
    
    * updates
    
    * react.sh
    
    * default port
    
    * clean out
    
    * standardize
    
    * restore
    
    * removed unused file
    
    * test
    
    * code review
    
    * back-end commands
    
    * withScope the POST request for transaction_id

--- src/components/App.js
+++ src/components/App.js
@@ -108,7 +108,9 @@ class App extends Component {
   }
 
   checkout() {
-    this.myCodeIsPerfect();
+
+    // this.myCodeIsPerfect();
+
 
     /*
       POST request to /checkout endpoint.
@@ -120,31 +122,33 @@ class App extends Component {
       cart: this.state.cart
     };
 
-    // generate unique transactionId and set as Sentry tag
-    const transactionId = getUniqueId();
-    Sentry.configureScope(scope => {
+    Sentry.withScope(scope => {
+      // generate unique transactionId and set as Sentry tag
+      const transactionId = getUniqueId();
       scope.setTag("transaction_id", transactionId);
+      console.log("TRANSACTION ID", transactionId)
+      // perform request (set transctionID as header and throw error appropriately)
+      request.post({
+          url: `http://localhost:${PORT}/checkout`,
+          json: order,
+          headers: {
+            "X-Session-ID": this.sessionId,
+            "X-Transaction-ID": transactionId
+          }
+        }, (error, response) => {
+          if (error) {
+            throw error;
+          }
+          if (response.statusCode === 200) {
+            this.setState({ success: true });
+          } else {
+            throw new Error(response.statusCode + " - " + (response.statusMessage || response.body));
+          }
+        }
+      );    
     });
-    // perform request (set transctionID as header and throw error appropriately)
-    request.post({
-        url: `http://localhost:${PORT}/checkout`,
-        json: order,
-        headers: {
-          "X-Session-ID": this.sessionId,
-          "X-Transaction-ID": transactionId
+    
 
-        }
-      }, (error, response) => {
-        if (error) {
-          throw error;
-        }
-        if (response.statusCode === 200) {
-          this.setState({ success: true });
-        } else {
-          throw new Error(response.statusCode + " - " + (response.statusMessage || response.body));
-        }
-      }
-    );
   }
 
   render() {
