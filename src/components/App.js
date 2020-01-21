0884473ce2ec115a1c91f6fc10de2ec06d5ffafe
Neil Manvar <neil@sentry.io>
2020-01-21 14:24:54 -0800

    Revert "remove hammer"
    This reverts commit 80f230a1856e9be28bf2f3b524e9c0fec2c7d7f9.

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,12 @@ class App extends Component {
         name: "Nails",
         price: 25,
         img: nailsImg
+      },
+      {
+        id: "hammer",
+        name: "Hammer",
+        price: 1000,
+        img: hammerImg
       }
     ];
     this.buyItem = this.buyItem.bind(this);
