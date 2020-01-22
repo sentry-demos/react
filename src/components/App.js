5e5cb3cd5693c17df48893fb0e0bb022d6515cef
Neil Manvar <neil@sentry.io>
2020-01-21 16:02:22 -0800

    Revert "one item remove"
    This reverts commit e9065a76eece395f78dd8919cb2be3d9c9581e20.

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,12 @@ class App extends Component {
 
     this.store = [
       {
+        id: "wrench",
+        name: "Wrench",
+        price: 500,
+        img: wrenchImg
+      },
+      {
         id: "nails",
         name: "Nails",
         price: 25,
