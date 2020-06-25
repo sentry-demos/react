d381619646d7530c8e9c1fda06c5fd634056ec90
Kelly Carino <kelly@sentry.io>
2020-06-24 18:54:34 -0700

    feat: Add Redux Logger
    Add Redux Logger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducers/app';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider> , document.getElementById('root'));
 
